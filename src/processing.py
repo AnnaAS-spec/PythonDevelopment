c4e3cd634b4648255c9472013514beb72a05e333
Anna <saushev.as@gmail.com>
2025-10-18 20:59:59 +0500

    add dev fylter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from typing import List, Dict
 
 
@@ -9,15 +8,3 @@ def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
 
     """
     return [item for item in data if item.get("state") == state]
-
-
-def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
-    """
-    Возвращает новый список словарей, отсортированный по дате (ключ 'date').
-
-    """
-    return sorted(
-        data,
-        key=lambda x: datetime.fromisoformat(x["date"]),
-        reverse=reverse
-    )
